aa6c9a4afc2a49bc9a4b902854bd4a407dc97b00
heber <heberlazo@gmail.com>
2022-01-04 18:59:34 -0500

    Added controller

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,32 @@
+class SubscribersController < ApplicationController
+  before_action :set_subscribe, only: [:create]
+  def index
+    @subscriber = Subscriber.new
+  end
+
+  def create
+    @sub = Subscriber.new(subscriber_params)
+    return other_mail if EmailValidationServices.validate(@sub.email) < 0.70
+    if @sub.save
+      UserMailer.with(subscriber: @sub).welcome_email.deliver_later
+      cookies[:saved_lead] = true
+      redirect_to root_path, notice: 'Saved Successfully!'
+    else
+      redirect_to root_path, notice: 'Already Subscribed!'
+    end
+  end
+
+  def other_mail
+    redirect_to root_path, notice: 'Please Provide another mail'
+  end
+
+  def set_subscribe
+    @subscriber = Subscriber.find_by(params[:email])
+  end
+
+  private
+
+  def subscriber_params
+    params.require(:subscriber).permit(:name, :email, :source)
+  end
+end
