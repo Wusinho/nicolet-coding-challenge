5f610d12c78c0552f681ec17a53f7de10eef01d9
heber <heberlazo@gmail.com>
2022-01-04 19:11:38 -0500

    Added checkbox to subscriber form

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,4 @@
 class Subscriber < ApplicationRecord
+
+PREFERENCES = ["Women's Fashion", "Men's Fashion", "Children's Fashion"]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'subscribers/index'
+  resources :subscribers
+  root 'subscribers#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
